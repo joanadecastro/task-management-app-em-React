813efe36301b7986348059ae02c4bdfd35eef3a6
Joana Castro <87642062+joanadecastro@users.noreply.github.com>
2024-10-28 10:00:20 +0000

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,45 @@ import { useState } from 'react'
 
 function App() {
 
+  const[todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade X no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para o ginasio",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ])
+
   return (
-    <div>Ola React</div>
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => {
+          return <div className="todo">
+            <div className="content">
+              <p>{todo.text}</p>
+              <p className="category">({todo.category})</p>
+            </div>
+            <div>
+              <button>Completar</button>
+              <button>X</button>
+            </div>
+          </div>
+        })}
+      </div>
+    </div>
   )
 }
 
