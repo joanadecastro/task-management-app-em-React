495e454c42f3baf7db2f462dc6cb32b01da6db4d
Joana Castro <87642062+joanadecastro@users.noreply.github.com>
2024-10-31 10:39:18 +0000

    formulario css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Todo from './components/Todo'
+import TodoForm from './components/TodoForm'
 import './App.css'
 
 
@@ -34,6 +35,7 @@ function App() {
           <Todo todo={todo}/>
         ))}
       </div>
+      <TodoForm />
     </div>
   )
 }
