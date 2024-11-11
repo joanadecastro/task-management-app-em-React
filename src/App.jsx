e16eb1d119d7fff51317b48781c950162be60ca4
Joana Castro <87642062+joanadecastro@users.noreply.github.com>
2024-11-11 18:40:57 +0000

    search

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Todo from './components/Todo'
 import TodoForm from './components/TodoForm'
+import Search from './components/Search'
 import './App.css'
 
 
